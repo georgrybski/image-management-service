da0591f637d7bbde46abb379e6132beb582dace4
georgrybski <rybskigeorg@gmail.com>
2023-03-18 23:34:31 -0300

    Added main class

---
+++ src/main/java/com/imagemanagementservice/ImageManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imagemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageManagementServiceApplication.class, args);
+    }
+
+}
